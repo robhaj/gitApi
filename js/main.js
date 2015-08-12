3d94b3c2a3cb3ee6c2f7d68b418d78865386928e
Robert Hajek <hajek.rob@gmail.com>
2015-08-11 21:37:30 -0600

    pulls two users, needs score comparison, stylings, and DRY refactor

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
-// split string function
-function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
-}
-
+//main function
 $(function(){
 
+  //player 1
+  //open click handler
   $('#ghsubmitbtn').on('click', function(e){
     e.preventDefault();
+
+    //loading gif
     $('#ghapidata').html('<div id="loader"><img src="css/loader.gif" alt="loading..."></div>');
 
-    var username = $('#ghusername').val();
-    var requri   = 'https://api.github.com/users/'+username;
-    var repouri  = 'https://api.github.com/users/'+username+'/repos';
+    //cache api urls and usernames
+    var username1 = $('#ghusername1').val();
+    var username2 = $('#ghusername2').val();
+    var requri1   = 'https://api.github.com/users/'+username1;
+    var requri2   = 'https://api.github.com/users/'+username2;
+    var repouri1  = 'https://api.github.com/users/'+username1+'/repos';
+    var repouri2  = 'https://api.github.com/users/'+username2+'/repos';
 
-    requestJSON(requri, function(json) {
-
-      if(json.message == "Not Found" || username === '') {
-        $('#ghapidata').html("<h2>No User Info Found</h2>");
-      } else {
+    //make ajax request1
+    requestJSON(requri1, function(json) {
+      //validation
+      if(json.message == "Not Found" || username1 === '') {
+        $('#ghapidata').html("<h2>One or more of the users were not found!</h2>");
 
         // else we have a user and we display their info
+      } else {
+
         var fullname   = json.name;
-        var username   = json.login;
+        var username1   = json.login;
         var aviurl     = json.avatar_url;
         var profileurl = json.html_url;
         var location   = json.location;
@@ -30,20 +35,19 @@ $(function(){
         var followingnum = json.following;
         var reposnum     = json.public_repos;
 
-        if(fullname === undefined) { fullname = username; }
+        //default name
+        if(fullname === undefined)
+        fullname = username1;
 
-        var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
-        outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
+        //structure output
+        var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username1+'</a>)</span></h2>';
+        outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username1+'"></a></div>';
         outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
         outhtml = outhtml + '<div class="repolist clearfix">';
         outhtml = outhtml + '<p>Location: '+location+'</p>';
 
+        //repo list
         var repositories;
-
-        $.getJSON(repouri, function(json){
-          repositories = json;
-          outputPageContent();
-        });
 
         //outputs content
         function outputPageContent() {
@@ -56,36 +60,94 @@ $(function(){
             outhtml = outhtml + '</ul></div>';
           }
           $('#ghapidata').html(outhtml);
-        } // end outputPageContent()
-      } // end else statement
-    }); // end requestJSON Ajax call
-  }); // end click event handler
 
-  $('#ghcodesearch').on('click', function(e){
-    e.preventDefault();
-    $('#search-results').html('<div id="loader"><img src="css/loader.gif" alt="loading..."></div>');
+        }
 
-    var language = $('#user-language').val();
-    var username = $('#ghusername').val();
-    var searchTerm = $('#searchTerm').val();
-    var codeSearch  = 'https://api.github.com/search/code?q='+searchTerm+'in:file+language:'+language+'+user:'+username;
-
-    requestJSON(codeSearch, function(json) {
-      var path = json.items[0].html_url;
-        path = setCharAt(path,7,'/raw.');
-        path = setCharAt(path,17, 'busercontent');
-        path = path.replace('blob/', '');
-        $.ajax({
-          url:path,
-          success: function(data) {
-            $('#search-results').html("Code: "+data);
-          }
+        $.getJSON(repouri1, function(json){
+          repositories = json;
+          outputPageContent();
         });
-    });
-  });
-});
 
-// //ajax request function
+        requestJSON(requri2, function(json) {
+
+          if(json.message == "Not Found" || username2 === '') {
+            $('#ghapidata').html("<h2>One or more of the users were not found!</h2>");
+
+          } else {
+            // else we have a user and we display their info
+            var fullname   = json.name;
+            var username2   = json.login;
+            var aviurl     = json.avatar_url;
+            var profileurl = json.html_url;
+            var location   = json.location;
+            var followersnum = json.followers;
+            var followingnum = json.following;
+            var reposnum     = json.public_repos;
+
+            if(fullname === undefined) { fullname = username2; }
+
+            var outhtml2 = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username2+'</a>)</span></h2>';
+            outhtml2 = outhtml2 + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username2+'"></a></div>';
+            outhtml2 = outhtml2 + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
+            outhtml2 = outhtml2 + '<div class="repolist clearfix">';
+            outhtml2 = outhtml2 + '<p>Location: '+location+'</p>';
+
+            var repositories2;
+
+
+
+            function outputPageContent2() {
+              if(repositories2.length === 0) { outhtml2 = outhtml2 + '<p>No repos!</p></div>'; }
+              else {
+                outhtml2 = outhtml2 + '<p><strong>Repos List:</strong></p> <ul>';
+                $.each(repositories2, function(index) {
+                  outhtml2 = outhtml2 + '<li><a href="'+repositories2[index].html_url+'" target="_blank">'+repositories2[index].name + '</a></li>';
+                });
+                outhtml2 = outhtml2 + '</ul></div>';
+              }
+
+              $('#ghapidata').append(outhtml2);
+
+            }//end outputPageContent2
+
+            $.getJSON(repouri2, function(json){
+              repositories2 = json;
+              outputPageContent2();
+            });
+          } // end else statement
+        }); // end requestJSON Ajax call
+      }//else end
+    });//end ajax call
+
+    //api to raw url modification
+
+    // requestJSON(codeSearch, function(json) {
+    //   var path = json.items[0].html_url;
+    //     path = setCharAt(path,7,'/raw.');
+    //     path = setCharAt(path,17, 'busercontent');
+    //     path = path.replace('blob/', '');
+    //     $.ajax({
+    //       url:path,
+    //       success: function(data) {
+    //         $('#search-results').html("Code: "+data);
+    //       }
+    //     });
+    // });
+
+  });//end event handler
+});//end main function
+
+
+
+
+// split string function
+function setCharAt(str,index,chr) {
+  if(index > str.length-1) return str;
+  return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+
+// reusable ajax request function
 function requestJSON(url, callback) {
   $.ajax({
     url: url,
