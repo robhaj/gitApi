092db134eb2b3650ddd63a42efb808dfcdf6b66a
Robert Hajek <hajek.rob@gmail.com>
2016-07-13 22:23:50 -0600

    edit file struc

--- js/utility.js
+++ js/utility.js
@@ -10,7 +10,7 @@ function requestJSON(url, callback) {
 
 //append loading gif function
 function loadingGif(id) {
-  var styles = '<div id="loader"><img src="../ass/img/loader.gif" alt="loading..."></div>';
+  var styles = '<div id="loader"><img src="ass/img/loader.gif" alt="loading..."></div>';
   return $(id).html(styles);
 }
 
@@ -36,7 +36,7 @@ function audioEvents() {
 
 //bullethole timings
 function appendBullets(id){
-    var bullet = '<img src="../ass/img/bullethole.png"></img>';
+    var bullet = '<img src="ass/img/bullethole.png"></img>';
     return $(id).html(bullet);
 }
 
